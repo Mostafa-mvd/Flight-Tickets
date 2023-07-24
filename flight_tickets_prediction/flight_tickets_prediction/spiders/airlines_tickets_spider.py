7268636c14f92b09c6e1d152051d7a29cf9f01c1
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-07-24 17:26:43 +0430

    added scrapy startproject files

---
+++ flight_tickets_prediction/flight_tickets_prediction/spiders/airlines_tickets_spider.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class AirlinesTickets(scrapy.Spider):
+    name = "airlines_tickets"
+
+    def start_requests(self):
+        pass
+
+    def parse(self, response, **kwargs):
+        pass
