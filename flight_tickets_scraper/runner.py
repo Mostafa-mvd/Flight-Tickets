15ad331ee3e3a25e400bf1c33052568d5b9ba175
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-07-27 00:50:32 +0430

    changed it for using it along side launch.json file for debugging project

--- flight_tickets_scraper/runner.py
+++ flight_tickets_scraper/runner.py
@@ -1,16 +1,17 @@
 import os
+import sys
 from scrapy.cmdline import execute
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+scrapy_file_name = sys.argv[1]
 
 try:
     execute(
         [
             'scrapy',
-            'crawl',
-            'SPIDER NAME',
-            '-o',
-            'out.json',
+            'runspider',
+            scrapy_file_name,
         ]
     )
 except SystemExit:
