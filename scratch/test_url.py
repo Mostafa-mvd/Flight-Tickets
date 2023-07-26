f05405a4bd618c33e5381c71f7c47814a1bde085
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-07-27 00:56:21 +0430

    changed for url testing

--- scratch/test_url.py
+++ scratch/test_url.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.tcharter.ir/tickets/tickets/VEhSLUtJSC0xNDAyLzA1LzA5/?airplane"
+url = 'https://www.tcharter.ir/tickets/tickets/VEhSLUlGTi0xNDAyLzA2LzIw/?airplane='
 #url = "https://www.tcharter.ir/tickets/get_agency/%5B1477%5D/THR/IFN/1690398000/"
 payload = {"tab": "airplane"}
 req = requests.post(url, data=payload)
 
-if req.status_code == 200:
+if req.content == b"error":
     soup = BeautifulSoup(req.content, 'html5lib')
     print(soup.prettify())
+else:
+    print(req.content)
