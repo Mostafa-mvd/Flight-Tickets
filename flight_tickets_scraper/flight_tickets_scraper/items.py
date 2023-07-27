260726e8f245edd55b01b86b314fe9ea4388695d
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-07-27 20:07:41 +0430

    added ticket fields to FligthTicketsScraperItem

--- flight_tickets_scraper/flight_tickets_scraper/items.py
+++ flight_tickets_scraper/flight_tickets_scraper/items.py
@@ -3,10 +3,17 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class FlightTicketsScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class FlightTicketsScraperItem(Item):
+    source = Field()
+    destination = Field()
+    company_name = Field()
+    arrival_time = Field()
+    capacity = Field()
+    flying_number = Field()
+    flying_class = Field()
+    ticket_price = Field()
+    flying_type = Field()
+    date = Field()
