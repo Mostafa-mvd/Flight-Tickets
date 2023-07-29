a9fb7a3930ce97d6ecfce755dd38b846881be07e
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-07-30 01:41:52 +0430

    Changed combination function to permutaion function

--- flight_tickets_scraper/flight_tickets_scraper/utils.py
+++ flight_tickets_scraper/flight_tickets_scraper/utils.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+from itertools import permutations
 
 from json import load
 import pathlib
@@ -22,19 +22,15 @@ def get_json_key_value(file_name, key):
     return airports_codes
 
 
-def make_two_combinations_airports():
-    def check_equal_constraint(airports_codes):
-        if (airports_codes[0], airports_codes[1]) != ("THR", "IKA"):
-            return True
-        return False
-
+def two_permutation_airports_codes():
     airports_city_codes_lst = get_json_key_value("airport_city_codes.json", "CityCodes")
+    
     airports_codes_lst = [airport_city_code_dict["id"]
                           for airport_city_code_dict in airports_city_codes_lst]
-    two_combinations_codes = filter(
-        check_equal_constraint, combinations(airports_codes_lst, 2))
+    
+    two_permutation_codes = permutations(airports_codes_lst, 2)
 
-    return two_combinations_codes
+    return two_permutation_codes
 
 
 def list_odd_values(lst):
