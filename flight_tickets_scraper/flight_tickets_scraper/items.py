a810e9bcaea42db3d4ede62672c1fbca03e2a1fb
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-02 00:26:51 +0430

    added new fields to FlightTicketsScraperItem and changed name of some of old fields

--- flight_tickets_scraper/flight_tickets_scraper/items.py
+++ flight_tickets_scraper/flight_tickets_scraper/items.py
@@ -8,13 +8,23 @@ from scrapy import Item, Field
 # Items act as simple temporary containers to store the extracted data before further processing or saving it.
 
 class FlightTicketsScraperItem(Item):
-    source = Field()
-    destination = Field()
-    company_name = Field()
+    # going out of source airport
+    national_departure_code = Field()
+
+    # coming into the destination airport
+    national_arrival_code = Field()
+
+    arrival_city_name_persian = Field()
+    departure_city_name_persian = Field()
+    
+    departure_time = Field()
     arrival_time = Field()
+    departure_date = Field()
+    departure_date_YMD_format = Field()
+
+    company_name = Field()
     capacity = Field()
     flying_number = Field()
     flying_class = Field()
-    ticket_price = Field()
+    ticket_price_T = Field()
     flying_type = Field()
-    date = Field()
