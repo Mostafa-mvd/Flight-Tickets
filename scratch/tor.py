1f6ed0b43eebd0d6c77d6db4a87eddf012fb8411
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-01 20:58:30 +0430

    Changed tor.py codes for testing

--- scratch/tor.py
+++ scratch/tor.py
@@ -4,7 +4,7 @@ import requests
 import re
 
 from stem import Signal
-from stem.process import launch_tor_with_config
+from stem.process import launch_tor_with_config, launch_tor
 from stem.control import Controller
 
 from toripchanger import TorIpChanger
@@ -19,12 +19,14 @@ def send_requests(url, proxies=None):
 def set_new_tor_ip1():
     SOCKS_PORT = '9050'
     CONTROL_PORT = '9051'
+    MAX_CIRCUIT_DIRTINESS = '20'
 
+    # This writes a temporary torrc to disk.
     tor_process = launch_tor_with_config(
         config={
             'SocksPort': SOCKS_PORT,
-            'ControlPort':  CONTROL_PORT,
-            'MaxCircuitDirtiness': '20',
+            'ControlPort': CONTROL_PORT,
+            'MaxCircuitDirtiness': MAX_CIRCUIT_DIRTINESS,
         },
         init_msg_handler=lambda line: print(line) if re.search(
             'Bootstrapped', line) else False,
@@ -43,6 +45,9 @@ def set_new_tor_ip2():
 
 
 def set_new_tor_ip3():
+    # # This writes a temporary torrc to disk.
+    # Controller used to talk with Tor and the circuit will be built by it.
+    # The ControlPort is the port to control the Tor instance.
     with Controller.from_port(port=9051) as controller:
         controller.authenticate(password="SigmaZ2015")
         controller.signal(Signal.NEWNYM)
@@ -56,7 +61,7 @@ urls = [
         'https://api.myip.com/',
     ]
 
-# Prixoxy for connecting your request to tor network
+# Privoxy for connecting your request to tor network
 PROXIES = {
         'http': '127.0.0.1:8118',
         'https': '127.0.0.1:8118',
