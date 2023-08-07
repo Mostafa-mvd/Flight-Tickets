796489efabffc23b27de3b4d3342e59eea38fcb4
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-07 21:01:33 +0430

    Added DuplicatesItemsPipeline and TicketArrivalTimePipeline
    DuplicatesItemsPipeline can help us for detecting duplicated items with ticket_id field and TicketArrivalTimePipeline checks arrival_time to be correct

--- flight_tickets_scraper/flight_tickets_scraper/pipelines.py
+++ flight_tickets_scraper/flight_tickets_scraper/pipelines.py
@@ -5,6 +5,7 @@
 
 
 # useful for handling different item types with a single interface
+from scrapy.exceptions import DropItem
 from itemadapter import ItemAdapter
 
 # If you need additional processing of the extracted data before saving it, you can use Scrapy pipelines. Pipelines allow you to perform tasks like data validation, cleaning, or saving to a database.
@@ -12,3 +13,28 @@ from itemadapter import ItemAdapter
 class FlightTicketsScraperPipeline:
     def process_item(self, item, spider):
         return item
+
+class TicketArrivalTimePipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        arrival_time = adapter.get("arrival_time")
+
+        if arrival_time:
+            splitted_arrival_time = arrival_time.split(":")
+
+            if splitted_arrival_time[0] == "ورود":
+                item["arrival_time"] = None
+
+        return item
+
+class DuplicatesItemsPipeline:
+    def __init__(self):
+        self.ids_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter["ticket_id"] in self.ids_seen:
+            raise DropItem(f"Duplicate item found:\n")
+        else:
+            self.ids_seen.add(adapter["ticket_id"])
+            return item
