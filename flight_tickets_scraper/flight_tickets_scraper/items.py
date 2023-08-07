ae9f341416617730f1aae1d652f86df6b7e10dc4
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-07 20:52:02 +0430

    added ticket_id field for uniquely specifying each items, this will help us for checks duplicated items

--- flight_tickets_scraper/flight_tickets_scraper/items.py
+++ flight_tickets_scraper/flight_tickets_scraper/items.py
@@ -8,6 +8,8 @@ from scrapy import Item, Field
 # Items act as simple temporary containers to store the extracted data before further processing or saving it.
 
 class FlightTicketsScraperItem(Item):
+    ticket_id = Field()
+    
     # going out of source airport
     national_departure_code = Field()
 
@@ -28,3 +30,7 @@ class FlightTicketsScraperItem(Item):
     flying_class = Field()
     ticket_price_T = Field()
     flying_type = Field()
+
+    def set_all_default_value(self, value=None):
+        for keys, _ in self.fields.items():
+            self[keys] = value
