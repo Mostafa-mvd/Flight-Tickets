d4512495564b6079e641cdc9fd8027204dac9aa5
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-14 02:16:47 +0430

    Added incomplete AIRPORTS_INFO_DIC

--- flight_tickets_prediction/required_data.py
+++ flight_tickets_prediction/required_data.py
@@ -15,6 +15,226 @@ MONTH_DICT = {
     "اسفند": "12",
 }
 
+AIRPORTS_INFO_DIC = {
+    "مشهد": {
+        "airport_code": "MHD",
+        "airport_names": ["Shahid Hashemi Nejad", "Hashemi Nejad", "Mashhad Hashemi Nejad", "Mashhad"],
+        "city_name": "Mashhad",
+        "region": "Razavi Khorasan",
+        "country_code": "IR"
+    },
+    "تهران": {
+        "airport_code": "THR",
+        "airport_names": ["Mehrabad"],
+        "city_name": "Tehran",
+        "region": "Tehran",
+        "country_code": "IR"
+    },
+    "تهران (امام خمینی)": {
+        "airport_code": "IKA",
+        "airport_names": ["Imam Khomeini"],
+        "city_name": "Tehran",
+        "region": "Tehran",
+        "country_code": "IR"
+    },
+    "اصفهان": {
+        "airport_code": "IFN",
+        "airport_names": ["Shahid Beheshti", "Beheshti", "Isfahan Beheshti", "Isfahan"],
+        "city_name": "Isfahan",
+        "region": "Isfahan",
+        "country_code": "IR"
+    },
+    "کیش": {
+        "airport_code": "KIH",
+        "airport_names": ["Kish"],
+        "city_name": "Kish Island",
+        "region": "Hormozgan",
+        "country_code": "IR"
+    },
+    "اهواز": {
+        "airport_code": "AWZ",
+        "airport_names": ["Soleimani", "Ahwaz Soleimani", "Ahwaz"],
+        "city_name": "Ahwaz",
+        "region": "Khuzestan",
+        "country_code": "IR"
+    },
+    "شیراز": {
+        "airport_code": "SYZ",
+        "airport_names": ["Shahid Dastghaib", "Dastghaib", "Shiraz Dastghaib", "Shiraz"],
+        "city_name": "Shiraz",
+        "region": "Fars",
+        "country_code": "IR"
+    },
+    "آبادان": {
+        "airport_code": "ABD",
+        "airport_names": ["Ayatollah Jami", "Jami", "Abadan Jami", "Abadan"],
+        "city_name": "Abadan",
+        "region": "Khuzestan",
+        "country_code": "IR"
+    },
+    "سهند": {
+        "airport_code": "ACP",
+        "airport_names": ["Maragheh", "Sahand"],
+        "city_name": "Maragheh",
+        "region": "East Azerbaijan",
+        "country_code": "IR"
+    },
+    "ازمیر": {
+        "airport_code": "ADB",
+        "airport_names": ["Izmir", "Adnan Menderes"],
+        "city_name": "Izmir",
+        "region": "Geziemir",
+        "country_code": "TR"
+    },
+    "اردبیل": {
+        "airport_code": "ADU",
+        "airport_names": ["Ardabil"],
+        "city_name": "Ardabil",
+        "region": "Ardabil",
+        "country_code": "IR"
+    },
+    "امیدیه": {
+        "airport_code": "AKW",
+        "airport_names": ["Omidiyeh", "Aghajari"],
+        "city_name": "Aghajari",
+        "region": "Khuzestan",
+        "country_code": "IR"
+    },
+    "آلماتی": {
+        "airport_code": "ALA",
+        "airport_names": ["Almaty"],
+        "city_name": "Almaty",
+        "region": "Almaty",
+        "country_code": "KZ"
+    },
+    "آنتالیا": {
+        "airport_code": "AYT",
+        "airport_names": ["Antalya"],
+        "city_name": "Antalya",
+        "region": "Antalya",
+        "country_code": "TR"
+    },
+    "یزد": {
+        "airport_code": "AZD",
+        "airport_names": ["Shahid Sadooghi", "Sadooghi", "Yazd Sadooghi", "Yazd"],
+        "city_name": "Yazd",
+        "region": "Yazd",
+        "country_code": "IR"
+    },
+    "بیروت": {
+        "airport_code": "BEY",
+        "airport_names": ["Rafic Hariri", "Beirut"],
+        "city_name": "Beirut",
+        "region": "Mont-Liban",
+        "country_code": "LB"
+    },
+    "بغداد": {
+        "airport_code": "BGW",
+        "airport_names": ["Baghdad"],
+        "city_name": "Baghdad",
+        "region": "Baghdad",
+        "country_code": "IQ"
+    },
+    "بدروم": {
+        "airport_code": "BJV",
+        "airport_names": ["Milas", "Bodrum"],
+        "city_name": "Bodrum",
+        "region": "Mugla",
+        "country_code": "TR"
+    },
+    "بدروم": {
+        "airport_code": "BJV",
+        "airport_names": ["Milas", "Bodrum"],
+        "city_name": "Bodrum",
+        "region": "Mugla",
+        "country_code": "TR"
+    },
+    "بندرعباس": {
+        "airport_code": "BND",
+        "airport_names": ["Bandar Abbas"],
+        "city_name": "Bandar Abbas",
+        "region": "Hormozgan",
+        "country_code": "IR"
+    },
+    "باتومی": {
+        "airport_code": "BUS",
+        "airport_names": ["Batumi"],
+        "city_name": "Batumi",
+        "region": "Adjara",
+        "country_code": "GE"
+    },
+    "بوشهر": {
+        "airport_code": "BUZ",
+        "airport_names": ["Bushehr"],
+        "city_name": "Bushehr",
+        "region": "Bushehr",
+        "country_code": "IR"
+    },
+    "گوانگ ژو": {
+        "airport_code": "CAN",
+        "airport_names": ["Baiyun", "Guangzhou"],
+        "city_name": "Guangzhou",
+        "region": "Guangdong",
+        "country_code": "CN"
+    },
+    "پاریس": {
+        "airport_code": "CDG",
+        "airport_names": ["Charles de Gaulle", "Roissy", "Paris"],
+        "city_name": "Paris",
+        "region": "Val d'Oise",
+        "country_code": "FR"
+    },
+    "دمشق": {
+        "airport_code": "DAM",
+        "airport_names": ["Damascus"],
+        "city_name": "Damascus",
+        "region": "Rif dimashq",
+        "country_code": "SY"
+    },
+    "دزفول": {
+        "airport_code": "DEF",
+        "airport_names": ["Dezful"],
+        "city_name": "Dezful",
+        "region": "Khuzestan",
+        "country_code": "IR"
+    },
+    "دنیزلی": {
+        "airport_code": "DNZ",
+        "airport_names": ["Cardak", "Denizli"],
+        "city_name": "Denizli",
+        "region": "Denizli",
+        "country_code": "TR"
+    },
+    "دبی": {
+        "airport_code": "DXB",
+        "airport_names": ["Dubai"],
+        "city_name": "Dubai",
+        "region": "Dubai",
+        "country_code": "AE"
+    },
+    "لف کوزا": {
+        "airport_code": "ECN",
+        "airport_names": ["Ercan"],
+        "city_name": "Lefkoşa",
+        "region": "Nicosia",
+        "country_code": "CY"
+    },
+    "آنکارا": {
+        "airport_code": "ESB",
+        "airport_names": ["Esenboga", "Ankara"],
+        "city_name": "Ankara",
+        "region": "Ankara",
+        "country_code": "TR"
+    },
+    "ایروان": {
+        "airport_code": "EVN",
+        "airport_names": ["Zvartnots", "Yerevan"],
+        "city_name": "Yerevan",
+        "region": "Armavir",
+        "country_code": "AM"
+    }
+}
+
 AIRPORT_CITIES_NAME_EN_LOCALIZATION_DICT = {
     "مشهد": "Mashhad",
     "اصفهان": "Isfahan",
@@ -79,5 +299,5 @@ AIRPORT_CITIES_NAME_EN_LOCALIZATION_DICT = {
     "نجف": "Najaf",
     "مسقط": "Muscat",
     "لار": "Lar",
-    "سن پترزبورگ": "St. Petersburg",
+    "سن پترزبورگ": "Saint Petersburg",
 }
