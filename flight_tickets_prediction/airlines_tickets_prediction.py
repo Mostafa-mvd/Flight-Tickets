8b8fbd1bef42719a68be8c4faf2e0a385a83b82a
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-15 02:16:17 +0430

    Added new functions

--- flight_tickets_prediction/airlines_tickets_prediction.py
+++ flight_tickets_prediction/airlines_tickets_prediction.py
@@ -2,24 +2,56 @@ import json
 from matplotlib import pyplot as plt
 
 
-def get_city_codes_dict(json_file_path):
-    city_codes_dict = dict()
-
+def get_json_obj(json_file_path):
     with open(json_file_path) as json_file_handler:
-        json_file = json.load(json_file_handler)
-        city_codes_lst = json_file["CityCodes"]
-    
-    for city_code_dict in city_codes_lst:
-        national_code, city_persian_name = city_code_dict.values()
-        city_codes_dict[national_code] = city_persian_name
-    
-    return city_codes_dict
+        json_obj = json.load(json_file_handler)
+    return json_obj
 
 
-def correct_field(mail_col_field, secondary_col_field):
-    if mail_col_field != secondary_col_field:
+def extract_values_from_json_obj(obj, key):
+    arr = []
+
+    def extract(obj, arr, key):
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
+
+def extract_value_from_json_obj(data, nested_keys):
+    if not nested_keys:
+        return data
+
+    if isinstance(data, dict):
+        data_keys = data.keys()
+
+        for key in nested_keys:
+            if key in data_keys:
+                return extract_value_from_json_obj(data[key], nested_keys[1:])
+            else:
+                return None
+    elif isinstance(data, list):
+        for dict_item in data:
+            return extract_value_from_json_obj(dict_item, nested_keys)
+
+
+def create_flatten_dict(keys, values):
+    return {key: value for key, value in zip(keys, values)}
+
+
+def correct_field(main_col_field, secondary_col_field):
+    if main_col_field != secondary_col_field:
         return secondary_col_field
-    return mail_col_field
+    return main_col_field
 
 
 def semi_space_correction(x, normalizer):
@@ -43,8 +75,13 @@ def update_departure_date_YMD_format_fields(data, x_col_name, y_col_name, months
     return correct_field(departure_date_YMD_formats_col_field, created_departure_date_YMD_formats)
 
 
+def difference_drop(df, *args):
+    """difference drop with column names that you will give on *args"""
+    return df.drop(columns=df.columns.difference([*args]), axis=1)
+
+
 def update_dependent_col(main_df, func, x_col_name, y_col_name, your_dict):
-    secondary_df = main_df.drop(columns=main_df.columns.difference([x_col_name, y_col_name]), axis=1)
+    secondary_df = difference_drop(main_df, x_col_name, y_col_name)
     # secondary_df = main_df[main_df.columns.intersection([x_col_name, y_col_name])]
 
     main_df[y_col_name] = secondary_df.apply(
@@ -55,12 +92,18 @@ def update_dependent_col(main_df, func, x_col_name, y_col_name, your_dict):
     return main_df
 
 
-def change_field_value_to_en(x_field, localization_dict):
-    return localization_dict[x_field]
+def change_city_names_to_en(x_field, airports_info_dict):
+    return extract_value_from_json_obj(airports_info_dict, [x_field, "city_name"])
+
+
+def get_city_airport_names(x_field, airports_info_dict):
+    return extract_value_from_json_obj(airports_info_dict, [x_field, "airport_names"])[-1]
 
 
 def move_columns(main_df, cols_dict):
-    for col_name, next_position in cols_dict.items():
+    sorted_cols_dict = dict(sorted(cols_dict.items(), key=lambda item: item[1]))
+
+    for col_name, next_position in sorted_cols_dict.items():
         column_to_move = main_df.pop(col_name)
         main_df.insert(next_position, col_name, column_to_move)
     return main_df
