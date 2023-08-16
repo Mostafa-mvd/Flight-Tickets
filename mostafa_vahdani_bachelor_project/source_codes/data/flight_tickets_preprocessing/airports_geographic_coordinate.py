75900766fe8f8200f7ed455e5afd2978db9a9eb3
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-16 17:22:39 +0430

    Updated script for getting geometry from API correctly

--- mostafa_vahdani_bachelor_project/source_codes/data/flight_tickets_preprocessing/airports_geographic_coordinate.py
+++ mostafa_vahdani_bachelor_project/source_codes/data/flight_tickets_preprocessing/airports_geographic_coordinate.py
@@ -7,20 +7,31 @@ from common_utils.utils import get_json_obj
 
 from settings import OPENCAGEDATA_API_KEY, SOURCES
 
+
 max_recursive_depth = 0
 
-def get_airport_geographic_coordinate(geo_coder, q, country_code, no_annotations=1, 
-                                      no_dedupe=1, pretty_output=1, language='en', airport_name=None):
+
+def get_airport_geographic_coordinate(geo_coder, **kwargs):
     """This function is just working on 'OpenCageGeocode web api' results for my specific task."""
     global max_recursive_depth
 
     if max_recursive_depth == 2:
         return None
     
+    q = kwargs["query"]
+    country_code = kwargs["country_code"]
+    airport_name = kwargs["airport_name"]
+
+    airport_code = kwargs.get("airport_code", q)
+    no_dedupe = kwargs.get("no_dedupe", 1)
+    no_annotations = kwargs.get("no_annotations", 1)
+    pretty_output = kwargs.get("pretty", 1)
+    language = "en"
+    
     result_lst = geo_coder.geocode(query=q, countrycode=country_code, no_dedupe=no_dedupe,
                                    no_annotations=no_annotations, pretty=pretty_output,
                                    language=language)
-    sleep(2)
+    sleep(1)
 
     for result_dict in result_lst:
         components_dict = result_dict["components"]
@@ -29,14 +40,20 @@ def get_airport_geographic_coordinate(geo_coder, q, country_code, no_annotations
         if (_category == "transportation" and 
             _type == "aeroway"):
 
-            airport_name = components_dict["aeroway"]
+            aeroway_name = components_dict.get("aeroway", airport_name)
 
-            return {airport_name: result_dict["geometry"]}
+            return {"aeroway_name": aeroway_name,
+                    "airport_code": airport_code,
+                    "geometry": result_dict["geometry"]}
         
     max_recursive_depth += 1
-    return get_airport_geographic_coordinate(geo_coder,
-                                             q=f"{airport_name} Airport",
-                                             country_code=country_code,)
+    
+    params = {"query": f"{airport_name} Airport",
+              "country_code": country_code,
+              "airport_code": airport_code,
+              "airport_name": airport_name}
+
+    return get_airport_geographic_coordinate(geo_coder, **params)
 
 
 if __name__ == "__main__":
@@ -45,14 +62,12 @@ if __name__ == "__main__":
     geo_coder = OpenCageGeocode(OPENCAGEDATA_API_KEY)
 
     for airport_val in airports_info_dict.values():
-        airport_code = airport_val["airport_code"]
-        country_code = airport_val["country_code"]
-        airport_name = airport_val["airport_names"][-1]
 
-        airport_geometry_dict = get_airport_geographic_coordinate(geo_coder, 
-                                                                  airport_code,
-                                                                  country_code,
-                                                                  airport_name=airport_name)
+        params = {"query": airport_val["airport_code"],
+                  "country_code": airport_val["country_code"],
+                  "airport_name": airport_val["airport_names"][-1]}
+
+        airport_geometry_dict = get_airport_geographic_coordinate(geo_coder, **params)
 
         if airport_geometry_dict:
             airports_geometry_lst.append(airport_geometry_dict)
