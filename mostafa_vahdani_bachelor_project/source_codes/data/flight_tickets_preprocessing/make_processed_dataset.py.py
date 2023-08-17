8d5711065778af1993d687fca359c0325cc07d7d
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-18 01:17:34 +0430

    Updated executing codes

--- mostafa_vahdani_bachelor_project/source_codes/data/flight_tickets_preprocessing/make_processed_dataset.py.py
+++ mostafa_vahdani_bachelor_project/source_codes/data/flight_tickets_preprocessing/make_processed_dataset.py.py
@@ -3,25 +3,25 @@ import pandas as pd
 
 from hazm import Normalizer
 
-from settings import (SOURCES, COLUMNS_NEED_TO_MOVE)
-
-from preprocessing import (change_city_names_to_en,
-                           get_city_airport_names,
+from preprocessing import (change_city_names_to_en, difference_drop,
+                           estimate_flight_length, estimate_arrival_time,
+                           orthodromic_distance, get_city_airport_names,
                            update_city_persian_name_fields,
                            update_departure_date_YMD_format_fields,
-                           update_dependent_col,
-                           semi_space_correction,
-                           move_columns,
-                           replace_with)
+                           update_dependent_col, semi_space_correction,
+                           move_columns, replace_with, 
+                           change_company_name_specific_value)
 
-from common_utils.utils import (get_json_obj, 
-                                extract_values_from_json_obj, 
-                                create_flatten_dict)
+from common_utils.utils import (get_json_obj, create_flatten_dict,
+                                extract_values_from_json_obj)
+
+from settings import (SOURCES, COLUMNS_NEED_TO_MOVE)
 
 
 MONTH_DICT = get_json_obj(SOURCES["months_json_file_path"])
 AIRPORT_CODES_DICT = get_json_obj(SOURCES["airport_codes_json_file_path"])
 AIRPORTS_INFO_DICT = get_json_obj(SOURCES["airports_info_json_file_path"])
+AIRPORTS_GEOMETRY_DICT = get_json_obj(SOURCES["airports_geometry_json_file_path"])
 
 CITY_AIRPORT_CODES_LST = extract_values_from_json_obj(AIRPORT_CODES_DICT, "id")
 CITY_AIRPORT_NAMES_FA_LST = extract_values_from_json_obj(AIRPORT_CODES_DICT, "city")
@@ -35,7 +35,7 @@ df = df.drop(["ticket_id"], axis=1)
 
 df = df.drop_duplicates()
 
-df["capacity"] = replace_with(df["capacity"], "موجود", np.nan, float)
+df["flight_capacity"] = replace_with(df["flight_capacity"], "موجود", np.nan, float)
 
 # Fill where fields of arrival_city_name_persian column are empty.
 df = update_dependent_col(
@@ -63,7 +63,7 @@ df = update_dependent_col(
     "departure_date_YMD_format",
     MONTH_DICT)
 
-df = df.sort_values(by=['departure_date_YMD_format', 'departure_time', 'arrival_time'], ascending=True)
+df = df.sort_values(by=['departure_date_YMD_format', 'local_departure_time', 'local_arrival_time'], ascending=True)
 
 # Change arrival city names from persian to english.
 df["arrival_city"] = df["arrival_city_name_persian"].apply(
@@ -86,9 +86,28 @@ df["departure_airport"] = df["departure_city_name_persian"].apply(
     args=(AIRPORTS_INFO_DICT,))
 
 # Deleting columns
-df = df.drop(["arrival_city_name_persian", "departure_city_name_persian"], axis=1)
+df = df.drop(["arrival_city_name_persian", 
+              "departure_city_name_persian",
+              "departure_date"], axis=1)
 
-#Move position of columns.
+# Add orthodromic distance col based on kilometer
+df2 = difference_drop(df, "national_departure_code", "national_arrival_code")
+
+df["orthodromic_distance_KM"] = df2.apply(func=orthodromic_distance,
+                                          args=(AIRPORTS_GEOMETRY_DICT,),
+                                          axis=1)
+
+# Add flight length col based on minutes
+df2 = difference_drop(df, "orthodromic_distance_KM")
+df["flight_length_min"] = df2.apply(func=estimate_flight_length, axis=1)
+
+# Estimating arrival_time col
+df["local_arrival_time"] = df.apply(func=estimate_arrival_time, axis=1)
+
+# Changing company values
+df["company_name"] = df["company_name"].apply(func=change_company_name_specific_value)
+
+# Move position of columns.
 df = move_columns(
     df,
     COLUMNS_NEED_TO_MOVE)
