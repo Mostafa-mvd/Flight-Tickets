6c53d2c9d90bc9e7c6bd65fa38c397158a9aab6d
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-18 01:16:55 +0430

    Added some new pre-processing functions

--- mostafa_vahdani_bachelor_project/source_codes/data/flight_tickets_preprocessing/preprocessing.py
+++ mostafa_vahdani_bachelor_project/source_codes/data/flight_tickets_preprocessing/preprocessing.py
@@ -1,6 +1,13 @@
+import pandas as pd
+import pytz
 
 from matplotlib import pyplot as plt
+
+from haversine import Unit, haversine
 from common_utils.utils import extract_value_from_json_obj
+
+from jdatetime import datetime, timedelta
+from random import randint
 
 
 def correct_field(main_col_field, secondary_col_field):
@@ -24,7 +31,7 @@ def update_city_persian_name_fields(data, x_col_name, y_col_name, city_codes_dic
 def update_departure_date_YMD_format_fields(data, x_col_name, y_col_name, months_dict):
     _, day_of_month, month = data[x_col_name].split()
     month_number_str = months_dict[month]
-    created_departure_date_YMD_formats = f"1402/{month_number_str}/{day_of_month}"
+    created_departure_date_YMD_formats = f"1402-{month_number_str}-{day_of_month}"
     departure_date_YMD_formats_col_field = data[y_col_name]
 
     return correct_field(departure_date_YMD_formats_col_field, created_departure_date_YMD_formats)
@@ -71,6 +78,78 @@ def replace_with(col_df, origin_value, replacement_value, type_value=None):
         col_df = col_df.astype(float)
     
     return col_df
+
+
+def orthodromic_distance(row, airports_geometry_dict):
+    departure_airport_code = row["national_departure_code"]
+    arrival_airport_code = row["national_arrival_code"]
+
+    departure_airport_coordinate = extract_value_from_json_obj(airports_geometry_dict, 
+                                                               [departure_airport_code, 
+                                                                "geometry"])
+    
+    arrival_airport_coordinate = extract_value_from_json_obj(airports_geometry_dict,
+                                                             [arrival_airport_code, 
+                                                              "geometry"])
+
+    return round(haversine(departure_airport_coordinate,
+                           arrival_airport_coordinate,
+                           Unit.KILOMETERS,
+                           normalize=True))
+
+
+def estimate_flight_length(distance):
+    v_km_per_min = randint(885, 965) / 60
+
+    # per minutes
+    taxi_time = randint(5, 10)
+    attach_stairs_time = randint(5, 10)
+    takeoff_clearance = randint(5, 10)
+    takeoff_time = randint(10, 20)
+    cruise_time = distance / v_km_per_min
+    vectoring_time = 10
+    busy_airplane_time = 10
+    descent_time = randint(10, 15)
+    landing_time = randint(10, 20)
+    
+    # TODO: I have to added layover time for direct and connecting flight
+
+    flight_length = descent_time + landing_time + takeoff_time + \
+                    taxi_time + attach_stairs_time + takeoff_clearance + \
+                    busy_airplane_time + vectoring_time + cruise_time
+
+    return round(flight_length)
+
+
+def estimate_arrival_time(row):
+    arrival_time = row["local_arrival_time"]
+    if pd.isnull(arrival_time):
+        local_departure_timezone = pytz.timezone('Asia/Tehran')
+        departure_time_hour, departure_time_minute = row["local_departure_time"].split(":")
+        year, month, day = row["departure_date_YMD_format"].split("-")
+        flight_length_hour = row["flight_length_min"] / 60
+        hour_part = int(flight_length_hour)
+        min_part = (flight_length_hour - hour_part) * 60
+
+        departure_time_obj = datetime(
+            year=int(year), month=int(month), day=int(day), hour=int(departure_time_hour),
+            minute=int(departure_time_minute), tzinfo=local_departure_timezone)
+
+        estimated_arrival_time = departure_time_obj + timedelta(hours=hour_part, minutes=min_part)
+        return datetime.strftime(estimated_arrival_time, '%H:%M')
+    return arrival_time
+
+
+def change_company_name_specific_value(series_value):
+    if 1 <= len(series_value) <= 3:
+        return series_value.upper()
+    elif series_value == "فری برد":
+        return "Freebird"
+    elif series_value == "ماوی گوک":
+        return "Mavi Gok"
+    elif len(series_value) > 3:
+        return series_value.capitalize()
+    return series_value
 
 
 def check_col_distribution(col_df):
