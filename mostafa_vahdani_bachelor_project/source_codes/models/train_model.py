4cf9752743cb0d5987aebf1e41d2331c71f49625
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2023-08-23 00:44:13 +0430

    Working on how to train my model

--- mostafa_vahdani_bachelor_project/source_codes/models/train_model.py
+++ mostafa_vahdani_bachelor_project/source_codes/models/train_model.py
@@ -1,16 +1,18 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+
+from sklearn.discriminant_analysis import StandardScaler
 from sklearn.model_selection import train_test_split
 
-processed_dataset_path = "/home/magnus9102/Mostafa/Py/Github/data-science/mostafa_vahdani_bachelor_project/data/processed/flight_tickets_dataset.csv"
 
-df = pd.read_csv(processed_dataset_path)
+final_dataset_path = "/home/magnus9102/Mostafa/Py/Github/data-science/mostafa_vahdani_bachelor_project/data/interim/final_flight_tickets_dataset.csv"
 
-X = df.drop(['ticket_price_T'], axis=1)
+df = pd.read_csv(final_dataset_path)
+
 y = df['ticket_price_T']
+X = df.drop('ticket_price_T', axis=1)
+
+s = StandardScaler()
+X = s.fit_transform(X)
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-linear_model = LinearRegression()
-
-linear_model.fit(x_train, y_train)
